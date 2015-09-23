89a12c553525368eec6ccc1be93444274c8ef9fa
Desafino <fc.olfert@hotmail.com>
2015-09-23 12:53:01 +0200

    GOLD!

--- TwitchHeroesApp/TwitchHeroes/www/js/click.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/click.js
@@ -2,7 +2,7 @@
  * Created by kenn3898 on 23-09-2015.
  */
 var ClickDamage = 1;
-var DamagePerSecond = 0;
+var DamagePerSecond = 12.5;
 
 
 (function($){
@@ -15,21 +15,21 @@ var image = $('#enemyImg');
     $(EnemyName).text(CurrentEnemy.name);
     $(HealthText).text(CurrentEnemy.health + " HP");
 
-    // DamagePerSecond function - runs 1/second
+    // DamagePerSecond function runs every 0.2 second
     setInterval(function(){
-        CurrentEnemy.health -= DamagePerSecond;
+        CurrentEnemy.health -= DamagePerSecond / 5;
         $(HealthText).text(CurrentEnemy.health + " HP");
-        $(HealthBar).css("border-right = " + CurrentEnemy.health / CurrentEnemy.MaxHealth +"px");
+        $(HealthBar).css("width", (CurrentEnemy.health / CurrentEnemy.MaxHealth) * 100 + "%");
         if (CurrentEnemy.health <= 0)
         {
             GiveMoney(Math.floor(CurrentEnemy.gold / 3));
             EnemyCounter();
             UpdateCounter();
-            $('#HealthBar').css("width","100%");
+            $(HealthBar).css("width","100%");
             $(EnemyName).text(CurrentEnemy.name);
             $(image).attr("src",CurrentEnemy.img);
         }
-    }, 1000);
+    }, 200);
 
 
         // when enemy is clicked
@@ -49,32 +49,32 @@ var image = $('#enemyImg');
 
 
         }
-
-            // creates visual money, that goes up when the enemy dies.
-            function GiveMoney(gold)
-            {
-                for (var i = 0; i < 3; i++)
-                {
-                // create ticket
-                var ticket="<div class='ticket'><p>+"+ gold +"</p></div>";
-                $(ticket).appendTo("body");
-                var ThisTicket = $('.ticket').last();
-                // get window dimentions
-                var ww = $(window).width();
-                var wh = $(window).height();
-                var posx = Math.floor(Math.random() * ww - 20);
-                var posy = (wh / 3);
-                ThisTicket.last().css("top", posy + "px").css("left", posx + "px");
-                ThisTicket.last()
+    });
+    // creates visual money, that goes up when the enemy dies.
+    function GiveMoney(gold)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            // create ticket
+            var ticket="<div class='ticket'><p>+"+ gold +"</p></div>";
+            $(ticket).appendTo("body");
+            var ThisTicket = $('.ticket').last();
+            // get window dimentions
+            var ww = $(window).width();
+            var wh = $(window).height();
+            var posx = Math.floor(Math.random() * ww - 20);
+            var posy = (wh / 3);
+            ThisTicket.last().css("top", posy + "px").css("left", posx + "px");
+            ThisTicket.last()
                 .animate({
                     opacity: 'hide',      // animate fade
                     top: 0        // animation slideUp
                 }, 1500, function() {
                     $(this).remove();
                 });
-            }
         }
-    });
+        UpdateMoney(gold * 3);
+    }
 
 
 
