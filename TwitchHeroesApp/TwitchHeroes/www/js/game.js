f26c849b6938e61d94916b81a8f28c27fa9b3fd7
TykkeJonas94 <pleaseblamemelol@gmail.com>
2015-09-23 09:31:06 +0200

    Det er features

--- TwitchHeroesApp/TwitchHeroes/www/js/game.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/game.js
@@ -2,4 +2,23 @@
  * Created by kenn3898 on 23-09-2015.
  */
 
+var Money = 0;
 var Level = 1;
+var currentEnemy = 0;
+var maxEnemy = 10;
+var enemyCounter = currentEnemy + " / " + maxEnemy;
+var CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.length)], Level * 10, Level * 200);
+
+
+
+(function($){
+
+    function EnemyCounter(){
+        CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.length)], Level * 10, Level * 200);
+        currentEnemy++;
+
+        if(currentEnemy >= maxEnemy){
+            Level++;
+        }
+    }
+})(jQuery);
