c9cee6e8c49d0b8f02f938797ff64baa06a64e93
TykkeJonas94 <pleaseblamemelol@gmail.com>
2015-09-23 12:26:15 +0200

    HVAD?!

--- TwitchHeroesApp/TwitchHeroes/www/js/game.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/game.js
@@ -6,7 +6,6 @@ var Money = 0;
 var Level = 1;
 var enemyCount = 0;
 var maxEnemy = 10;
-var enemyCounter = enemyCount + " / " + maxEnemy;
 var CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.length)], Level * 10, Level * 200);
 
 
@@ -19,14 +18,14 @@ var CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.
 
     window.UpdateLevel = function(){
         Level++;
-
+        console.log(Level);
         $('#levelP').replaceWith("<p id='#levelP'>Level: "+ Level +"</p>");
     };
 
     window.UpdateCounter = function(){
         enemyCount++;
-
-        $('#counterP').replaceWith("<p id='#counterP'>"+ enemyCounter +"</p>");
+        console.log(enemyCount);
+        $('#counterP').replaceWith("<p id='#counterP'>"+ enemyCount +" / "+ maxEnemy +"</p>");
     };
 
 })(jQuery);
@@ -34,16 +33,12 @@ var CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.
 function EnemyCounter(){
 
     CurrentEnemy = new MakeEnemy(EnemyList[Math.floor(Math.random() * EnemyList.length)], Level * 10, Level * 200);
-    enemyCount++;
-
+    jQuery('#HealthBar').css('width',"100%");
     if(enemyCount >= maxEnemy){
         UpdateLevel();
     }
 }
 
-function UpdateCounter(){
-
-}
 
 function UpdateGold(){
 
