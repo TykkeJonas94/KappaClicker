9906bdfb60a6619fd45509089b046b0110f10450
TykkeJonas94 <pleaseblamemelol@gmail.com>
2015-09-23 15:08:08 +0200

    asdasdasd

--- TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
@@ -2,6 +2,11 @@
  * Created by jona796m on 23-09-2015.
  */
 
-(function($) {
-    //Kenneth er en faggot!
-})(jQuery);
+var UpgradeList = [{upgName:"FailFish", upgPrice:"1337", upgEffectName:"+10 Damage per second", upgEffect:"10"}];
+
+function upgrade(upgName, upgPrice, upgEffectName, upgEffect){
+    this.upgName = upgName;
+    this.upgPrice = upgPrice;
+    this.upgEffectName = upgEffectName;
+    this.upgEffect = upgEffect;
+}
