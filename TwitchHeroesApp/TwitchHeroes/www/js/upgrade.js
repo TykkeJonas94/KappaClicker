d4b356d8b8cf3379a53834aa276c890d00aef3cc
TykkeJonas94 <pleaseblamemelol@gmail.com>
2015-09-25 08:57:55 +0200

    Walla

--- TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
@@ -5,16 +5,17 @@
 /**Upgrade Effect Value 0 = +damagePerClick */
 /**Upgrade Effect Value     1 = +damagePerSecond */
 
-var UpgradeList = [ {upgName:"upgrade1", upgPrice:10, upgEffectName:"+1 Damage per Click", upgEffectValue:0, upgEffect:1},
-                    {upgName:"upgrade2", upgPrice:40, upgEffectName:"+1 Damage per Second", upgEffectValue:1, upgEffect:1}
+var UpgradeList = [ {upgName:"upgrade1", upgPrice:10, upgEffectName:"+1 Damage per Click", upgEffectValue:0, upgEffect:1, upgQuantity:0},
+                    {upgName:"upgrade2", upgPrice:40, upgEffectName:"+1 Damage per Second", upgEffectValue:1, upgEffect:1, upgQuantity:0}
 ];
 
-function upgrade(upgName, upgPrice, upgEffectName, upgEffectValue, upgEffect){
+function upgrade(upgName, upgPrice, upgEffectName, upgEffectValue, upgEffect, upgQuantity){
     this.upgName = upgName;
     this.upgPrice = upgPrice;
     this.upgEffectName = upgEffectName;
     this.upgEffectValue = upgEffectValue;
     this.upgEffect = upgEffect;
+    this.upgQuantity = upgQuantity;
 }
 
 (function($){
@@ -27,6 +28,7 @@ function upgrade(upgName, upgPrice, upgEffectName, upgEffectValue, upgEffect){
 
             $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgName +'</p>');
             $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgEffectName +'</p>');
+            $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgQuantity +'</p>');
             $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgPrice +'</p>');
         }
     })
