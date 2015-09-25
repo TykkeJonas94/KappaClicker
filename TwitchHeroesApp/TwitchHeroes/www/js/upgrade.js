17a7104bcd1e0bdd3a46998649a1c2c11d1b2edc
TykkeJonas94 <pleaseblamemelol@gmail.com>
2015-09-25 09:45:40 +0200

    UPLOAD HEDDER DET

--- TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
+++ TwitchHeroesApp/TwitchHeroes/www/js/upgrade.js
@@ -2,8 +2,9 @@
  * Created by jona796m on 23-09-2015.
  */
 
-/**Upgrade Effect Value 0 = +damagePerClick */
+/**Upgrade Effect Value     0 = +damagePerClick */
 /**Upgrade Effect Value     1 = +damagePerSecond */
+
 
 var UpgradeList = [ {upgName:"upgrade1", upgPrice:10, upgEffectName:"+1 Damage per Click", upgEffectValue:0, upgEffect:1, upgQuantity:0},
                     {upgName:"upgrade2", upgPrice:40, upgEffectName:"+1 Damage per Second", upgEffectValue:1, upgEffect:1, upgQuantity:0}
@@ -19,17 +20,37 @@ function upgrade(upgName, upgPrice, upgEffectName, upgEffectValue, upgEffect, up
 }
 
 (function($){
-    $(document).on('click','#shop',function(){
-        for(var i = 0; i < UpgradeList.length; i++){
-            console.log("fgt!");
+    for(var i = 0; i < UpgradeList.length; i++){
+        $('#upgradeList').append('<div class="effects" id="effects'+ i +'">');
+        $('#upgradeList').append('</div>');
 
-            $('#upgradeList').append('<div class="effects" id="effects'+ i +'">');
-            $('#upgradeList').append('</div>');
+        $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgName +'</p>');
+        $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgEffectName +'</p>');
+        $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgQuantity +'</p>');
+        $('#effects'+ i).append('<p class="listItem upgBuy" id="e'+ i +'">'+ UpgradeList[i].upgPrice +'</p>');
+    }
 
-            $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgName +'</p>');
-            $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgEffectName +'</p>');
-            $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgQuantity +'</p>');
-            $('#effects'+ i).append('<p class="listItem">'+ UpgradeList[i].upgPrice +'</p>');
+    $(document).on('click','.upgBuy',function(){
+        var upgradeID = $(this).attr('id');
+        upgradeID = upgradeID.replace('e', '');
+
+        if(Money > UpgradeList[upgradeID].upgPrice) {
+            UpgradeList[upgradeID].upgQuantity++;
+            UpdateMoney(-$(this).text());
+
+            if (UpgradeList[upgradeID].upgEffectValue == 0) {
+                ClickDamage += UpgradeList[upgradeID].upgEffect;
+
+                console.log(ClickDamage);
+            }
+            else if (UpgradeList[upgradeID].upgEffectValue == 1) {
+                DamagePerSecond += UpgradeList[upgradeID].upgEffect;
+
+                console.log(DamagePerSecond);
+            }
+            else {
+                console.log("Get fucked!");
+            }
         }
-    })
+    });
 })(jQuery);
